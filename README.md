271801f205f410632b21e37fb59319221519cbef
teohm <teohuiming@gmail.com>
2011-12-19 20:29:33 +0800

    setup project dir

---
+++ README.md
@@ -0,0 +1,3 @@
+== Being Rails
+
+a short project to define a set of minimum gems to speed up new Rails projects.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeingRails::Application.config.secret_token = '597557276f96442b13e81dbab51a449b3e48ea70855360039e2a25397f615159b4047ed01f313fc7b425ac30621636c92711c7ac4af5d9808f35dc7aa7bc312a'
